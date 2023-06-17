47d2d4278bc229d82c0cd274de2b36c5d48abc59
TheRouletteBoi <therouletteboi@outlook.com>
2023-06-17 13:12:43 -0700

    (Refactor); Delete unwanted files

--- libPowerObfuscator/src/Internal.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <types.h>
-#include "libPowerObfuscator.h"
-
-void pobf_SetApiKey(char *pKey);
-void pobf_SetApiKey(int32_t key);
-void pobf_DecryptFunction(int pFunction);
-void pobf_DecryptData();
-int32_t pobf_HashFile32();
--- libPowerObfuscator/src/Private.h
+++
@@ -1,8 +0,0 @@
-#include <sys/syscall.h>
-#include <sys/process.h>
-
-#define PRIVATE __attribute__((visibility("hidden")))
-
-unsigned int pobf_GenerateHash32(char *pStr);
-void pobf_Write(int pAddress, int pData, int pLength = sizeof(int));
-bool pobf_Skip(int pOperation);
