fb7a48ed32b4a1bac91413ef5a204fc82cd90cf7
TheRouletteBoi <therouletteboi@outlook.com>
2023-06-21 16:38:52 -0700

    Rename namespaces

--- libPowerObfuscator/src/PowerObfuscator.cpp
+++ libPowerObfuscator/src/PowerObfuscator.cpp
@@ -2,134 +2,132 @@
 
 namespace pobf
 {
-    char PRIV_API_KEY[] = "TEST KEY XXX";
-
-    int EXPORTS_TOC[] =
-    {
-       START_PATTERN
-    };
-
     uint32_t g_Seed = 0;
 
-    void SetApiKey(uint32_t key)
+    namespace EncryptV1
     {
-        g_Seed = key;
-    }
+        char PRIV_API_KEY[] = "TEST KEY XXX";
 
-    uint32_t MixTimeSeed(clock_t a, time_t b, sys_pid_t c)
-    {
-        a = a - b;  a = a - c;  a = a ^ (c >> 13);
-        b = b - c;  b = b - a;  b = b ^ (a << 8);
-        c = c - a;  c = c - b;  c = c ^ (b >> 13);
-        a = a - b;  a = a - c;  a = a ^ (c >> 12);
-        b = b - c;  b = b - a;  b = b ^ (a << 16);
-        c = c - a;  c = c - b;  c = c ^ (b >> 5);
-        a = a - b;  a = a - c;  a = a ^ (c >> 3);
-        b = b - c;  b = b - a;  b = b ^ (a << 10);
-        c = c - a;  c = c - b;  c = c ^ (b >> 15);
-        return c;
-    }
-
-    uint32_t StringToHash32(const char* str)
-    {
-        uint32_t hash = 0;
-        for (uint8_t* p = (uint8_t*)str; *p != '\0'; p++)
-            hash = (37 * hash) + *p;
-
-        return hash;
-    }
-
-    void todo_SeedRandom(uint32_t seed)
-    {
-
-    }
-
-    int todo_Random()
-    {
-        return 0;
-    }
-
-    void Start()
-    {
-        todo_SeedRandom(MixTimeSeed(clock(), time(NULL), sys_process_getpid()));
-
-        uint32_t random = todo_Random();
-        uint32_t keyHash = StringToHash32(PRIV_API_KEY);
-
-        SetApiKey(random + keyHash);
-
-        DecryptAll();
-    }
-
-    void DecryptAll()
-    {
-        sys_prx_segment_info_t segment = GetModuleSegmentInfo(nullptr, TEXT_SEGMENT);
-
-        for (int i = 0; i < segment.memsz; i++)
-            DecryptTextSegment(segment.base);
-
-        DecryptDataSegment();
-    }
-
-    void DecryptTextSegment(uint32_t function)
-    {
-        // function = ((opd_s*)function)->func;  // segment.base doesn't have opd section???
-        pobf_Random<0x11111111, 0x66666666, 0x7FFFFFFF> random(g_Seed);
-
-        uint32_t instruction = 0;
-        uint32_t key = 0;
-        do
+        int EXPORTS_TOC[] =
         {
-            key = random.Next();
-            instruction = *(uint32_t*)function; // get instructions
-            if (!Skip(instruction))
-            {
-                int code = instruction ^ key;
-                pobf_write_process_memory((void*)function, &code, 4); // PPC64 instructions are 4 bytes in length 
-            }
-            function += 0x04;
-        } while (instruction != (0x4E800020 ^ key));
-    }
-
-    void DecryptDataSegment()
-    {
-        sys_prx_segment_info_t segment = GetModuleSegmentInfo(nullptr, DATA_SEGMENT);
-        uint32_t* ptr = (uint32_t*)segment.base;
-
-        for (int i = 0; i < segment.memsz; i++, ptr++)
-        {
-            uint32_t code = *ptr ^ (segment.base * (i + 1));
-            pobf_write_process_memory(ptr, &code, 4);
-        }
-    }
-
-    bool Skip(uint32_t instruction)
-    {
-        char skip_bytes[] =
-        {
-           0x30,
-           0x3C,
-           0x80,
-           0xF8
+           START_PATTERN
         };
 
-        int size = sizeof(skip_bytes) / sizeof(skip_bytes[0]);
-        for (int i = 0; i < size; i++)
+        void SetApiKey(uint32_t key)
         {
-            if ((instruction >> 24) == skip_bytes[i])
-                return true;
+            g_Seed = key;
         }
 
-        return false;
+        uint32_t MixTimeSeed(clock_t a, time_t b, sys_pid_t c)
+        {
+            a = a - b;  a = a - c;  a = a ^ (c >> 13);
+            b = b - c;  b = b - a;  b = b ^ (a << 8);
+            c = c - a;  c = c - b;  c = c ^ (b >> 13);
+            a = a - b;  a = a - c;  a = a ^ (c >> 12);
+            b = b - c;  b = b - a;  b = b ^ (a << 16);
+            c = c - a;  c = c - b;  c = c ^ (b >> 5);
+            a = a - b;  a = a - c;  a = a ^ (c >> 3);
+            b = b - c;  b = b - a;  b = b ^ (a << 10);
+            c = c - a;  c = c - b;  c = c ^ (b >> 15);
+            return c;
+        }
+
+        uint32_t StringToHash32(const char* str)
+        {
+            uint32_t hash = 0;
+            for (uint8_t* p = (uint8_t*)str; *p != '\0'; p++)
+                hash = (37 * hash) + *p;
+
+            return hash;
+        }
+
+        void todo_SeedRandom(uint32_t seed)
+        {
+
+        }
+
+        int todo_Random()
+        {
+            return 0;
+        }
+
+        void Start(int suppressParameter)
+        {
+            todo_SeedRandom(MixTimeSeed(clock(), time(NULL), sys_process_getpid()));
+
+            uint32_t random = todo_Random();
+            uint32_t keyHash = StringToHash32(PRIV_API_KEY);
+
+            SetApiKey(random + keyHash);
+
+            DecryptAll();
+        }
+
+        void DecryptAll()
+        {
+            sys_prx_segment_info_t segment = GetModuleSegmentInfo(nullptr, TEXT_SEGMENT);
+
+            for (int i = 0; i < segment.memsz; i++)
+                DecryptTextSegment(segment.base);
+
+            DecryptDataSegment();
+        }
+
+        void DecryptTextSegment(uint32_t function)
+        {
+            // function = ((opd_s*)function)->func;  // segment.base doesn't have opd section???
+            pobf_Random<0x11111111, 0x66666666, 0x7FFFFFFF> random(g_Seed);
+
+            uint32_t instruction = 0;
+            uint32_t key = 0;
+            do
+            {
+                key = random.Next();
+                instruction = *(uint32_t*)function; // get instructions
+                if (!Skip(instruction))
+                {
+                    int code = instruction ^ key;
+                    pobf_write_process_memory((void*)function, &code, 4); // PPC64 instructions are 4 bytes in length 
+                }
+                function += 0x04;
+            } while (instruction != (0x4E800020 ^ key));
+        }
+
+        void DecryptDataSegment()
+        {
+            sys_prx_segment_info_t segment = GetModuleSegmentInfo(nullptr, DATA_SEGMENT);
+            uint32_t* ptr = (uint32_t*)segment.base;
+
+            for (int i = 0; i < segment.memsz; i++, ptr++)
+            {
+                uint32_t code = *ptr ^ (segment.base * (i + 1));
+                pobf_write_process_memory(ptr, &code, 4);
+            }
+        }
+
+        bool Skip(uint32_t instruction)
+        {
+            char skip_bytes[] =
+            {
+               0x30,
+               0x3C,
+               0x80,
+               0xF8
+            };
+
+            int size = sizeof(skip_bytes) / sizeof(skip_bytes[0]);
+            for (int i = 0; i < size; i++)
+            {
+                if ((instruction >> 24) == skip_bytes[i])
+                    return true;
+            }
+
+            return false;
+        }
     }
 
 
-
-
-
-
-
-    namespace Encrypt
+    namespace EncryptV2
     {
         bool DataCompare(const uint8_t* pbData, const uint8_t* pbMask, const char* szMask)
         {
@@ -179,7 +177,7 @@ namespace pobf
 
         void DecryptFunction(uint8_t* data, uint32_t startIndex, uint32_t endIndex, bool quick)
         {
-            static uint8_t key[57];
+            static uint8_t key[57]{};
             key[0] = 0x32;
             key[1] = 0x11;
             key[2] = 0x07;
@@ -257,7 +255,7 @@ namespace pobf
         }
 
 
-        namespace DefaultEncrypt
+        namespace Default
         {
             void __encryptFunctionStart(void* function, bool quick)
             {
@@ -285,7 +283,7 @@ namespace pobf
             void _encryptFunctionStart(void* function, bool quick)
             {
                 __encryptFunctionStart(function, quick);
-                DefaultPatternStart();
+                StartatternDefault();
             }
 
             void __encryptFunctionEnd(void* function, bool quick, bool deleteData)
@@ -319,12 +317,12 @@ namespace pobf
 
             void _encryptFunctionEnd(void* function, bool quick, bool deleteData)
             {
-                DefaultPatternEnd();
+                EndPatternDefault();
                 __encryptFunctionEnd(function, quick, deleteData);
             }
         }
 
-        namespace QuickEncrypt
+        namespace Quick
         {
             void __encryptFunctionStart(void* function, uint8_t* saveBuffer, uint32_t* start, uint32_t* end)
             {
@@ -364,18 +362,18 @@ namespace pobf
             void _encryptFunctionStart(void* function, uint8_t* saveBuffer, uint32_t* start, uint32_t* end)
             {
                 __encryptFunctionStart(function, saveBuffer, start, end);
-                QuickPatternStart();
+                StartPatternQuick();
             }
 
             void _encryptFunctionEnd(uint8_t* saveBuffer, uint32_t start, uint32_t end)
             {
-                QuickPatternEnd();
+                EndPatternQuick();
                 pobf_write_process_memory((void*)start, saveBuffer, end - start); // copy over the encrypted contents back over the function
 
             }
         }
 
-        namespace InlineEncrypt
+        namespace Inline
         {
             void __encryptFunctionStart(void* function)
             {
@@ -400,7 +398,7 @@ namespace pobf
             void _encryptFunctionStart(void* function)
             {
                 __encryptFunctionStart(function);
-                DefaultPatternStart();
+                StartatternDefault();
             }
 
             void __encryptFunctionEnd(void* function)
@@ -424,7 +422,7 @@ namespace pobf
 
             void _encryptFunctionEnd(void* function)
             {
-                DefaultPatternEnd();
+                EndPatternDefault();
                 __encryptFunctionEnd(function);
             }
         }
--- libPowerObfuscator/src/PowerObfuscator.h
+++ libPowerObfuscator/src/PowerObfuscator.h
@@ -25,126 +25,81 @@ namespace pobf
         bool found;
     };
 
+    extern uint32_t g_Seed;
+
     #define START_PATTERN 0xAABBCCDD, 0x12345678, 0xEEFFEEFF 
 
-    extern char PRIV_API_KEY[];
-    extern int EXPORTS_TOC[];
-
-    void        POBF_API    SetApiKey(uint32_t key);
-    uint32_t    POBF_API    MixTimeSeed(clock_t a, time_t b, sys_pid_t c);
-    void        POBF_API    todo_SeedRandom(uint32_t seed);
-    int         POBF_API    todo_Random();
-    uint32_t    POBF_API    StringToHash32(const char* str);
-    void        POBF_API    Start();
-    void        POBF_API    DecryptAll();
-    void        POBF_API    DecryptTextSegment(uint32_t function);
-    void        POBF_API    DecryptDataSegment();
-    bool        POBF_API    Skip(uint32_t instruction);
-
-
-
-
-
-
-
-
-
-    namespace Encrypt
+    namespace EncryptV1
     {
-        uint32_t moduleTextSegmentStartCache = 0;
-        uint32_t moduleTextSegmentEndCache = 0;
+        extern char PRIV_API_KEY[];
+        extern int EXPORTS_TOC[];
 
+        void        POBF_API    SetApiKey(uint32_t key);
+        uint32_t    POBF_API    MixTimeSeed(clock_t a, time_t b, sys_pid_t c);
+        void        POBF_API    todo_SeedRandom(uint32_t seed);
+        int         POBF_API    todo_Random();
+        uint32_t    POBF_API    StringToHash32(const char* str);
+        void        POBF_API    Start(int suppressParameter);
+        void        POBF_API    DecryptAll();
+        void        POBF_API    DecryptTextSegment(uint32_t function);
+        void        POBF_API    DecryptDataSegment();
+        bool        POBF_API    Skip(uint32_t instruction);
+    }
 
-        ALWAYS_INLINE bool DataCompare(const uint8_t* pbData, const uint8_t* pbMask, const char* szMask);
-        ALWAYS_INLINE bool FindPattern(uintptr_t address, uint32_t length, uint8_t step, uint8_t* bytes, const char* mask, uint32_t* foundOffset);
-        ALWAYS_INLINE void FindPatternsInParallel(uintptr_t address, uint32_t length, std::vector<Pattern>& patterns, std::vector<uint32_t>& foundOffsets);
-        ALWAYS_INLINE void DecryptFunction(uint8_t* data, uint32_t startIndex, uint32_t endIndex, bool quick = false);
+    namespace EncryptV2
+    {
+        bool POBF_API DataCompare(const uint8_t* pbData, const uint8_t* pbMask, const char* szMask);
+        bool POBF_API FindPattern(uintptr_t address, uint32_t length, uint8_t step, uint8_t* bytes, const char* mask, uint32_t* foundOffset);
+        void POBF_API FindPatternsInParallel(uintptr_t address, uint32_t length, std::vector<Pattern>& patterns, std::vector<uint32_t>& foundOffsets);
+        void POBF_API DecryptFunction(uint8_t* data, uint32_t startIndex, uint32_t endIndex, bool quick = false);
 
-        namespace DefaultEncrypt
+        namespace Default
         {
-            void __encryptFunctionStart(void* function, bool quick);
-            ALWAYS_INLINE void _encryptFunctionStart(void* function, bool quick);
-            void __encryptFunctionEnd(void* function, bool quick, bool deleteData = false);
-            ALWAYS_INLINE void _encryptFunctionEnd(void* function, bool quick, bool deleteData = false);
+            void POBF_API __encryptFunctionStart(void* function, bool quick);
+            void POBF_API _encryptFunctionStart(void* function, bool quick);
+            void POBF_API __encryptFunctionEnd(void* function, bool quick, bool deleteData = false);
+            void POBF_API _encryptFunctionEnd(void* function, bool quick, bool deleteData = false);
         }
 
-        namespace QuickEncrypt
+        namespace Quick
         {
-            void __encryptFunctionStart(void* function, uint8_t* saveBuffer, uint32_t* start, uint32_t* end);
-            ALWAYS_INLINE void _encryptFunctionStart(void* function, uint8_t* saveBuffer, uint32_t* start, uint32_t* end);
-            ALWAYS_INLINE void _encryptFunctionEnd(uint8_t* saveBuffer, uint32_t start, uint32_t end);
+            void POBF_API __encryptFunctionStart(void* function, uint8_t* saveBuffer, uint32_t* start, uint32_t* end);
+            void POBF_API _encryptFunctionStart(void* function, uint8_t* saveBuffer, uint32_t* start, uint32_t* end);
+            void POBF_API _encryptFunctionEnd(uint8_t* saveBuffer, uint32_t start, uint32_t end);
         }
 
-        namespace InlineEncrypt
+        namespace Inline
         {
-            ALWAYS_INLINE void __encryptFunctionStart(void* function);
-            ALWAYS_INLINE void _encryptFunctionStart(void* function);
-            ALWAYS_INLINE void __encryptFunctionEnd(void* function);
-            ALWAYS_INLINE void _encryptFunctionEnd(void* function);
+            void POBF_API __encryptFunctionStart(void* function);
+            void POBF_API _encryptFunctionStart(void* function);
+            void POBF_API __encryptFunctionEnd(void* function);
+            void POBF_API _encryptFunctionEnd(void* function);
         }
 
+        /******* For pattern scanning ********/
+#define StartatternDefault() \
+            __asm("xor %r3, %r3, %r4;"       /* \x7C\x63\x22\x78 */    \
+                    "xor %r3, %r3, %r4;");   /* \x7C\x63\x22\x78 */
 
+#define EndPatternDefault() \
+            __asm("xor %r3, %r3, %r5;"       /* \x7C\x63\x2A\x78 */   \
+                    "xor %r3, %r3, %r5;");   /* \x7C\x63\x2A\x78 */
 
-        /******* default Encrypt ********/
-#define encryptFunctionStart(function) {} \
-            void *local_encrypt_function_address = function; \
-            DefaultEncrypt::_encryptFunctionStart(local_encrypt_function_address, false);
+#define StartPatternQuick() \
+            __asm("xor %r3, %r3, %r6;"       /* \x7C\x63\x32\x78 */   \
+                    "xor %r3, %r3, %r6;");   /* \x7C\x63\x32\x78 */
 
-#define encryptFunctionEnd() {} \
-            DefaultEncrypt::_encryptFunctionEnd(local_encrypt_function_address, false);
+#define EndPatternQuick() \
+            __asm("xor %r3, %r3, %r7;"       /* \x7C\x63\x3A\x78 */   \
+                    "xor %r3, %r3, %r7;");   /* \x7C\x63\x3A\x78 */
 
-#define encryptFunctionEnd_deletedata() {} \
-            DefaultEncrypt::_encryptFunctionEnd(local_encrypt_function_address, false, true);
+#define StartPattternThread() \
+            __asm("lis %r3, 0xFFEE;"          /* \x3C\x60\xFF\xEE */ \
+                    "ori %r3, %r3, 0xDDCC;"); /* \x60\x63\xDD\xCC */
 
-
-
-        /******* quickEncrypt ********/
-#define encryptFunctionStart_quick(function,bufferSize) {} \
-            void *local_encrypt_function_address = function; \
-            static uint8_t encryptionBuffer[bufferSize]; \
-            static uint32_t startEncryptAddress = 0; \
-            static uint32_t endEncryptAddress = 0; \
-            QuickEncrypt::_encryptFunctionStart(function, encryptionBuffer, &startEncryptAddress, &endEncryptAddress);
-
-#define encryptFunctionEnd_quick() {} \
-            QuickEncrypt::_encryptFunctionEnd(encryptionBuffer, startEncryptAddress, endEncryptAddress);
-
-
-        /******* inlineEncrypt ********/
-#define inline_encryptFunctionStart(function) {} \
-            void *local_encrypt_function_address = function; \
-            InlineEncrypt::_encryptFunctionStart(local_encrypt_function_address);
-
-#define inline_encryptFunctionEnd() {} \
-            InlineEncrypt::_encryptFunctionEnd(local_encrypt_function_address);
-
-
-
-
-        /******* Patterns to scan for ********/
-#define DefaultPatternStart() \
-            __asm("xor %r3, %r3, %r4"       /* \x7C\x63\x22\x78 */    \
-                    "xor %r3, %r3, %r4");   /* \x7C\x63\x22\x78 */
-
-#define DefaultPatternEnd() \
-            __asm("xor %r3, %r3, %r5"       /* \x7C\x63\x2A\x78 */   \
-                    "xor %r3, %r3, %r5");   /* \x7C\x63\x2A\x78 */
-
-#define QuickPatternStart() \
-            __asm("xor %r3, %r3, %r6"       /* \x7C\x63\x32\x78 */   \
-                    "xor %r3, %r3, %r6");   /* \x7C\x63\x32\x78 */
-
-#define QuickPatternEnd() \
-            __asm("xor %r3, %r3, %r7"       /* \x7C\x63\x3A\x78 */   \
-                    "xor %r3, %r3, %r7");   /* \x7C\x63\x3A\x78 */
-
-#define ThreadPattternStart() \
-            __asm("lis %r3, 0xFFEE"          /* \x3C\x60\xFF\xEE */ \
-                    "ori %r3, %r3, 0xDDCC"); /* \x60\x63\xDD\xCC */
-
-#define ThreadPatternEnd() \
-            __asm("lis %r3, 0xFFEE"          /* \x3C\x60\xFF\xEE */ \
-                    "ori %r3, %r3, 0xDDCD"); /* \x60\x63\xDD\xCD */
+#define EndPatternThread() \
+            __asm("lis %r3, 0xFFEE;"          /* \x3C\x60\xFF\xEE */ \
+                    "ori %r3, %r3, 0xDDCD;"); /* \x60\x63\xDD\xCD */
 
 
 
