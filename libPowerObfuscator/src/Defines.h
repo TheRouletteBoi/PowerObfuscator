393690ed498eef3283fe8b29442b2e3330d69111
TheRouletteBoi <therouletteboi@outlook.com>
2023-06-21 00:41:23 -0700

    Add ALWAYS_INLINE & NO_INLINE

--- libPowerObfuscator/src/Defines.h
+++ libPowerObfuscator/src/Defines.h
@@ -3,6 +3,9 @@
 // So PowerObfuscator symbols/functions don't show up in the PowerObfuscatorGUI App
 #define POBF_API /* __attribute__((visibility("hidden"))) GCC Only */
 
+#define ALWAYS_INLINE static inline  __attribute__((always_inline))
+#define NO_INLINE                           __attribute__((noinline))
+
 template<int A, int C, int M>
 class pobf_Random
 {
