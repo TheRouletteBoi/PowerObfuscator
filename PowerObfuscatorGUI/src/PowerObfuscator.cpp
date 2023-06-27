8b902b2b38e3e7202888c2b11bc98a671f472def
TheRouletteBoi <therouletteboi@outlook.com>
2023-06-26 17:15:42 -0700

    GUI: basic functionality

--- PowerObfuscatorGUI/src/PowerObfuscator.cpp
+++ PowerObfuscatorGUI/src/PowerObfuscator.cpp
@@ -8,4 +8,56 @@ PowerObfuscator::PowerObfuscator(QWidget *parent)
 
 PowerObfuscator::~PowerObfuscator()
 {}
-     
+
+void PowerObfuscator::on_loadPrxButton_clicked()
+{
+    const QString fileName = QFileDialog::getOpenFileName(this, "Open PRX File", m_currentDir.absolutePath(), "PRX File (*.prx)");
+
+    if (fileName.isEmpty())
+        return;
+
+    openFile(fileName);
+}
+
+void PowerObfuscator::openFile(const QString& fileName)
+{
+    QFile file(fileName);
+
+    m_doesfileExist = file.exists();
+    if (!m_doesfileExist)
+        return;
+
+    QFileInfo fileInfo(file);
+    m_currentDir = fileInfo.dir();
+    m_fileSize = fileInfo.size();
+    ui.filePathComboBox->addItem(fileName);
+
+    std::string fileNameStdString = fileName.toStdString();
+    getElfInfo(fileNameStdString);
+    getSizeStatistics(fileNameStdString);
+}
+
+void PowerObfuscator::on_showInfoButton_clicked()
+{
+    QString text = "No file loaded";
+
+    if (m_doesfileExist)
+    {
+        std::string elfFlags = "Yes";
+        std::string isObfuscated = "Yes";
+        std::string format = std::format("ELF Flags: {0}\nObfuscated: {1}\nFile Size: {2}\n", elfFlags, isObfuscated, m_fileSize);
+        text = QString::fromStdString(format);
+    }
+
+    QMessageBox::information(this, windowTitle(), text);
+}
+
+void PowerObfuscator::on_obfuscateButton_clicked()
+{
+
+}
+
+void PowerObfuscator::on_deobfuscateButton_clicked()
+{
+
+}
--- PowerObfuscatorGUI/src/PowerObfuscator.h
+++ PowerObfuscatorGUI/src/PowerObfuscator.h
@@ -1,7 +1,24 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <bit>
+#include <concepts>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+#include <cstdio>
+
 #include <QtWidgets/QMainWindow>
+#include <QDir>
+#include <QFileInfo>
+#include <QFileDialog>
+#include <QMessageBox>
 #include "ui_PowerObfuscator.h"
+
+#include "PrxInfo.h"
 
 class PowerObfuscator : public QMainWindow
 {
@@ -11,6 +28,18 @@ public:
     PowerObfuscator(QWidget *parent = nullptr);
     ~PowerObfuscator();
 
+public slots:
+    void openFile(const QString& fileName);
+
+private slots:
+    void on_loadPrxButton_clicked();
+    void on_showInfoButton_clicked();
+    void on_obfuscateButton_clicked();
+    void on_deobfuscateButton_clicked();
+
 private:
     Ui::PowerObfuscatorClass ui;
+    QDir m_currentDir;
+    bool m_doesfileExist{};
+    quint64 m_fileSize{};
 };
