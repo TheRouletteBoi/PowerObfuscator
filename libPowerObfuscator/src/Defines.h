75d3a11b64af813518639291221732e70efe7ea0
TheRouletteBoi <therouletteboi@outlook.com>
2023-07-20 21:17:54 -0700

    LIB: Update define name

--- libPowerObfuscator/src/Defines.h
+++ libPowerObfuscator/src/Defines.h
@@ -1,3 +1,3 @@
 #pragma once
-#define ALWAYS_INLINE static inline  __attribute__((always_inline))
-#define NO_INLINE                    __attribute__((noinline))
+#define STATIC_ALWAYS_INLINE static inline  __attribute__((always_inline))
+#define NO_INLINE                           __attribute__((noinline))
